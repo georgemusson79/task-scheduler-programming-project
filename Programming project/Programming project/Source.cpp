bbed70680fd7d1492ceb97ac3c038e2ac90616bc
george <george@DESKTOP-13OGO0A>
2024-02-28 12:08:43 +0000

    test2

--- Programming project/Programming project/Source.cpp
+++ Programming project/Programming project/Source.cpp
@@ -1,4 +1,8 @@
 #include <iostream>
+#define SDL_MAIN_HANDLED
+#include <SDL.h>
 int main() {
-	std::cout << "test";
+	SDL_Init(SDL_INIT_EVERYTHING);
+	SDL_Window* w = SDL_CreateWindow("Task Scheduler", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 500, 500, SDL_WINDOW_SHOWN);
+	while (1);
 }
