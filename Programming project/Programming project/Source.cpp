59085489082386c3ef3fba1310d159f6556ea73a
george <george@DESKTOP-13OGO0A>
2024-02-28 12:28:58 +0000

    allow user to quit

--- Programming project/Programming project/Source.cpp
+++ Programming project/Programming project/Source.cpp
@@ -6,8 +6,18 @@
 #include "Vector2.h"
 
 int main() {
+	bool running = true;
 	SDL_Init(SDL_INIT_EVERYTHING);
 	Vector2 scrDims=Main::getDisplayDims();
 	SDL_Window* w = SDL_CreateWindow("Task Scheduler", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, scrDims.X/2, scrDims.Y/2, SDL_WINDOW_SHOWN);
-	while (1);
+	SDL_Event e;
+	while (running) {
+		while (SDL_PollEvent(&e)) {
+			switch (e.type) {
+			case SDL_QUIT:
+				SDL_Quit();
+				running = false;
+			}
+		}
+	}
 }
