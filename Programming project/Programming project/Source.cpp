e1a0b113582f9bc321c4b9a03b6266ecd7afe132
george <george@DESKTOP-13OGO0A>
2024-02-28 12:14:35 +0000

    added vector2 struct

--- Programming project/Programming project/Source.cpp
+++ Programming project/Programming project/Source.cpp
@@ -1,6 +1,9 @@
-#include <iostream>
 #define SDL_MAIN_HANDLED
+#include <iostream>
 #include <SDL.h>
+
+#include "Vector2.h"
+
 int main() {
 	SDL_Init(SDL_INIT_EVERYTHING);
 	SDL_Window* w = SDL_CreateWindow("Task Scheduler", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 500, 500, SDL_WINDOW_SHOWN);
